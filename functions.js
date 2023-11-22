eb6180525f091a71836ae2811bed152fe0e662b5
[gareth hines] <gareth_hines@hotmail.com>
2023-11-22 19:29:12 +0000

    Add arrays storing the random strings

---
+++ functions.js
@@ -0,0 +1,5 @@
+let weatherType = ['rainy', 'cold', 'snowy', 'windy', 'sunny', 'hot', 'warm', 'humid', 'stormy'];
+let planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+let clothes = ['Hat', 'Coat', 'Scarf', 'Jacket', 'Boots', 'Wellies', 'Sandals', 'Sunglasses'];
+
+
